122bed41ff0f2907b13d0748a583c72194f20aa8
Seyi Adeniyi <adeniyiemmanuel042@gmail.com>
2024-06-30 22:03:56 +0100

    new update

--- app/components/(fronte)/products/MobileMenu.jsx
+++ app/components/(fronte)/products/MobileMenu.jsx
@@ -69,7 +69,7 @@ export function MobileMenu({toggleSidebar,isOpen,sessions, cartItems}) {
                 <>
                 <Link href={'/login'}><Sidebar.Item  className="text-sm flex justify-center">
                 <button class="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-pink-500 to-orange-400 group-hover:from-pink-500 group-hover:to-orange-400 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-pink-200 dark:focus:ring-pink-800">
-<span class="relative px-12 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
+<span class="relative px-16 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
 Sign in
 </span>
 </button>
